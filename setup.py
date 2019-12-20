cb91372d3808758e93c5ba3f7df74551946ff7c3
Conor ONeill <cwjoneill@gmail.com>
2019-12-20 12:45:06 +0000

    altair

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name='nodeconfeu_watch',
           'seaborn',
           'tensorflow',
           'matplotlib',
+          'altair',
           'flatbuffers'
       ],
       test_suite='nose.collector',
